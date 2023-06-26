829ab958a0275657e48c1c2b1a6685be45b99726
Jhan Frank <jhnfrankz@gmail.com>
2023-06-26 17:52:53 -0500

    Added CORS and Validation annotations from API Java Bean Validation

--- src/main/java/com/jhnfrankz/backend/usersapp/backendusersapp/models/entities/User.java
+++ src/main/java/com/jhnfrankz/backend/usersapp/backendusersapp/models/entities/User.java
@@ -6,6 +6,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -15,12 +19,18 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // esta anotacion es para que no se repita el username, solo funciona cuando se
-    // crea la tabla por primera vez
+    /* esta anotacion es para que no se repita el username, solo funciona cuando se crea la tabla por primera vez */
+    /* validamos que el username no este vacio y que tenga entre 4 y 8 caracteres, estas validaciones se hacen en el backend, es decir, en el servidor al momento de recibir la peticion del cliente (frontend), por ejemplo si el usuario no envia el username, el backend le responde con un error 400, si el usuario envia un username con menos de 4 caracteres, el backend le responde con un error 400, si el usuario envia un username con mas de 8 caracteres, el backend le responde con un error 400, si el usuario envia un username que ya existe, el backend le responde con un error 400, si el usuario envia un username valido, el backend le responde con un codigo 201 */
+    @NotBlank
+    @Size(min = 4, max = 8)
     @Column(unique = true)
     private String username;
+    
+    @NotEmpty
     private String password;
 
+    @NotEmpty
+    @Email
     @Column(unique = true)
     private String email;
 
