edad956e4ae26f8cfa07658f8ecb47f51ff28ae9
Jhan Frank <jhnfrankz@gmail.com>
2023-07-08 18:39:09 -0500

    Login with JPA

--- src/main/java/com/jhnfrankz/backend/usersapp/backendusersapp/repositories/UserRepository.java
+++ src/main/java/com/jhnfrankz/backend/usersapp/backendusersapp/repositories/UserRepository.java
@@ -1,9 +1,21 @@
 package com.jhnfrankz.backend.usersapp.backendusersapp.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.jhnfrankz.backend.usersapp.backendusersapp.models.entities.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
-    // ya no es necesario crear los metodos, ya que se hereda de CrudRepository
+    // ya no es necesario crear los metodos como: save, findById, findAll, deleteById
+    // porque CrudRepository ya los tiene
+
+    // Método con palabras clave de Spring Data JPA
+    Optional<User> findByUsername(String username);
+
+    // Método con JPQL, es como SQL pero con las entidades de Java
+    // el ?1 es el primer parametro que se le pasa al metodo
+    @Query("select u from User u where u.username=?1")
+    Optional<User> getUserByUsername(String username);
 }
--- src/main/java/com/jhnfrankz/backend/usersapp/backendusersapp/services/JpaUserDetailsService.java
+++ src/main/java/com/jhnfrankz/backend/usersapp/backendusersapp/services/JpaUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.jhnfrankz.backend.usersapp.backendusersapp.services;
 
+import com.jhnfrankz.backend.usersapp.backendusersapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -7,24 +9,35 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-// el UserDetailsService se encarga de buscar al usuario en la base de datos por detrás
 // Los @Service son componentes de Spring que se encargan de la logica de negocio
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository repository;
+
     // el UserDetailsService se encarga de buscar al usuario en la base de datos por detrás
     // y luego se usa el AuthenticationManager para autenticar al usuario con el UserDetailsService
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
+        // Porqué se usa el User de Spring Security y no el User de la entidad User?
+        // porque el User de Spring Security implementa UserDetails que es una interfaz
+        // que se encarga de manejar la seguridad de los usuarios
+        Optional<com.jhnfrankz.backend.usersapp.backendusersapp.models.entities.User> o = repository.findByUsername(username);
         //simulamos que buscamos el usuario en la base de datos
-        if (!username.equals("admin")) {
+        if (!o.isPresent()) {
             throw new UsernameNotFoundException(String.format("Username %s no existe en el sistema", username));
         }
+
+        com.jhnfrankz.backend.usersapp.backendusersapp.models.entities.User user = o.orElseThrow();
 
         List<GrantedAuthority> authorities = new ArrayList<>();
         // siempre los roles deben empezar con ROLE_
@@ -33,8 +46,8 @@ public class JpaUserDetailsService implements UserDetailsService {
         // retornamos un UserDetails que es una clase de Spring Security
         //luego por detras esto pasa al AuthenticationManager y este se encarga de autenticar al usuario
         // la clave es 12345 encriptada con bcrypt
-        return new User(username,
-                "$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS",
+        return new User(user.getUsername(),
+                user.getPassword(),
                 true,
                 true,
                 true,
