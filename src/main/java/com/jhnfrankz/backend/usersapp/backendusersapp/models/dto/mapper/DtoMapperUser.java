2dfd2d612ee8efbca387baa7002d2a2e99a27be3
Jhan Frank <jhnfrankz@gmail.com>
2023-07-11 13:10:01 -0500

    DTO Mapper Builder

--- src/main/java/com/jhnfrankz/backend/usersapp/backendusersapp/models/dto/mapper/DtoMapperUser.java
+++ src/main/java/com/jhnfrankz/backend/usersapp/backendusersapp/models/dto/mapper/DtoMapperUser.java
@@ -1,15 +1,34 @@
 package com.jhnfrankz.backend.usersapp.backendusersapp.models.dto.mapper;
 
+import com.jhnfrankz.backend.usersapp.backendusersapp.models.dto.UserDto;
+import com.jhnfrankz.backend.usersapp.backendusersapp.models.entities.User;
+
 public class DtoMapperUser {
     // Patrón de diseño builder
 
     private static DtoMapperUser mapper;
 
+    private User user;
+
     private DtoMapperUser() {
     }
 
-    public static DtoMapperUser getInstance() {
+    public static DtoMapperUser builder() {
         mapper = new DtoMapperUser();
         return mapper;
     }
+
+    public DtoMapperUser setUser(User user) {
+        this.user = user;
+        return mapper;
+    }
+
+    public UserDto build() {
+        if (user == null) {
+            throw new RuntimeException("Debe pasar el entity user");
+        }
+
+        return new UserDto(this.user.getId(), this.user.getUsername(), this.user.getEmail());
+    }
+
 }
